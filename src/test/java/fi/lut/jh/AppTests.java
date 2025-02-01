acf38c9c481a933b19e9798bfa807b85607d3a2f
Jani Heinikoski <heinikoski.jani@gmail.com>
2025-02-01 20:28:28 +0200

    Remove comments

--- src/test/java/fi/lut/jh/AppTests.java
+++ src/test/java/fi/lut/jh/AppTests.java
@@ -4,9 +4,6 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 import org.junit.jupiter.api.Test;
 
-/**
- * Unit tests for App.java
- */
 class AppTests {
     @Test
     void appIsNotNull() {
